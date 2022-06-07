617b751694a4826c29d9928b974a9d1fa5a641c1
SyedzadaAbrar <105579901+SyedzadaAbrar@users.noreply.github.com>
2022-06-07 22:42:50 +0530

    Update my_script.sh

--- my_script.sh
+++ my_script.sh
@@ -1,7 +1,7 @@
-echo "Carbon big by syed abrar;
-echo "type g to enter";
+echo "Carbon bug by syed abrar"
+echo "type g to enter"
 read input 
 if [ $input == "g" ]
 then 
-    cd my_scrip.sh
+    bash my_scrip.sh
     fi
